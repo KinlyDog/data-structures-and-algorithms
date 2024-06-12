a65a06fb4d7d7b8d55c0ad4dc677fc31da912c1a
Andrey <kinlydog@gmail.com>
2024-06-12 21:27:49 +1000

    Added 'level' field if STN class

--- src/main/java/part02/simpletreenode/SimpleTreeNode.java
+++ src/main/java/part02/simpletreenode/SimpleTreeNode.java
@@ -6,10 +6,12 @@ public class SimpleTreeNode<T> {
     public T nodeValue;
     public SimpleTreeNode<T> parent;
     public List<SimpleTreeNode<T>> children;
+    public int level;
 
     public SimpleTreeNode(T nodeValue, SimpleTreeNode<T> parent) {
         this.nodeValue = nodeValue;
         this.parent = parent;
         this.children = null;
+        this.level = -1;
     }
 }
